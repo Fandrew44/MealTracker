20582ad070f2247b2c890bf8d02fe98e3a257d74
Fandrew44 <ahgreeting@gmail.com>
2021-05-31 21:31:33 -0600

    add food gif menu background

--- app/src/components/AppNav.jsx
+++ app/src/components/AppNav.jsx
@@ -4,11 +4,20 @@ import '../styles/AppNav.css';
 
 //NOTE: Parent components are affected by CSS styles applied to CHILD Components
 
+//NOTE: ALWAYS aim to MODULARIZE components so that they can be easily re-used & re-applied for different situations
+// - e.g. turn navName into a PROP rather than hard-coding its value here
+
 function AppNav({ navName }) {
+    const history = useHistory();
+
+    function returnToMenu() {
+        history.push('/');
+    }
+
     return(
         <div class="navbar">
             <h2 id="navName" class="nav-element">{navName}</h2>
-            <h2 class="nav-element">Menu</h2>
+            <h2 class="nav-element menu-button" onClick={() => returnToMenu()}>Menu</h2>
         </div>
     );
 }
